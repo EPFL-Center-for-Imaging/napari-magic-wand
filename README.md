5e02b4ba8604fb66f0480837c4a93e7589b9082b
Mallory Wittwer <mallory.wittwer@epfl.ch>
2023-10-29 12:14:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# 🪄 napari-magic-wand
+
+Object annotation in Napari using a *magic wand* (shortest path algorithm). This plugin is based on the [PyIFT](https://github.com/PyIFT/pyift) library. This tool supports annotation in 2D (grayscale and RGB), 2D+t (grayscale, frame by frame) and 3D images (slice by slice). It can be used to annotate **paths** or solid objects via their contour.
+
+<p align="center">
+    <img src="https://github.com/MalloryWittwer/napari-magic-wand/blob/main/assets/screenshot.gif" height="400">
+</p>
+
+Please also take a look at the [grabber-ift](https://www.napari-hub.org/plugins/grabber-ift) plugin, which offers similar functionality but slightly different user interactions.
+
+## Installation
+
+You can install `napari-magic-wand` via [pip]:
+
+    pip install napari-magic-wand
+
+## Usage
+
+- Select the plugin from the `Plugins` menu of Napari.
+- Open an image to annotate (2D, 2D+t, or 3D).
+- Click on the button "Start live wire" or press `S`. A new `Labels` layer *Live wire (current edit)* should appear.
+- With the *Live wire* layer selected, click on the image to annotate the contour of an object.
+- Double-click to close the annotation path and fill the inside of the annotated object.
+
+## Contributing
+
+Contributions are very welcome. Please get in touch if you'd like to be involved in improving or extending the package.
+
+## License
+
+Distributed under the terms of the [BSD-3] license,
+"napari-magic-wand" is free and open source software
+
+## Issues
+
+If you encounter any problems, please file an issue along with a detailed description.
+
+----------------------------------
+
+This [napari] plugin was generated with [Cookiecutter] using [@napari]'s [cookiecutter-napari-plugin] template.
+
+[napari]: https://github.com/napari/napari
+[Cookiecutter]: https://github.com/audreyr/cookiecutter
+[@napari]: https://github.com/napari
+[MIT]: http://opensource.org/licenses/MIT
+[BSD-3]: http://opensource.org/licenses/BSD-3-Clause
+[GNU GPL v3.0]: http://www.gnu.org/licenses/gpl-3.0.txt
+[GNU LGPL v3.0]: http://www.gnu.org/licenses/lgpl-3.0.txt
+[Apache Software License 2.0]: http://www.apache.org/licenses/LICENSE-2.0
+[Mozilla Public License 2.0]: https://www.mozilla.org/media/MPL/2.0/index.txt
+[cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
+
+[napari]: https://github.com/napari/napari
+[tox]: https://tox.readthedocs.io/en/latest/
+[pip]: https://pypi.org/project/pip/
+[PyPI]: https://pypi.org/
---
+++ src/napari_magic_wand/_widget.py
@@ -0,0 +1,319 @@
+import napari
+import napari.layers
+from PyQt5.QtCore import Qt
+from qtpy.QtWidgets import (
+    QWidget, 
+    QComboBox, 
+    QSizePolicy, 
+    QLabel, 
+    QGridLayout, 
+    QPushButton,
+    QCheckBox,
+)
+import scipy.ndimage as ndi
+import pyift.livewire
+import numpy as np
+from napari.utils.notifications import show_info
+
+class LiveWireWidget(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+
+        self.viewer = napari_viewer
+
+        self.image_layer = None
+        self.labels_layer = None
+        self.result_layer = None
+        self.wire = None
+        self.is_active = False
+
+        # Layout
+        grid_layout = QGridLayout()
+        grid_layout.setAlignment(Qt.AlignTop)
+        self.setLayout(grid_layout)
+
+        # Image
+        self.cb_image = QComboBox()
+        self.cb_image.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(QLabel("Image", self), 0, 0)
+        grid_layout.addWidget(self.cb_image, 0, 1)
+
+        # Result
+        self.cb_result = QComboBox()
+        self.cb_result.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(QLabel("Labels (result)", self), 1, 0)
+        grid_layout.addWidget(self.cb_result, 1, 1)
+
+        grid_layout.addWidget(QLabel("Auto-increment label index", self), 2, 0)
+        self.check_label_increment = QCheckBox()
+        self.check_label_increment.setChecked(False)
+        grid_layout.addWidget(self.check_label_increment, 2, 1)
+
+        grid_layout.addWidget(QLabel("Close and fill objects", self), 3, 0)
+        self.auto_fill_objects_cb = QCheckBox()
+        self.auto_fill_objects_cb.setChecked(True)
+        grid_layout.addWidget(self.auto_fill_objects_cb, 3, 1)
+
+        # Push button
+        self.btn = QPushButton('Start live wire (s)', self)
+        self.btn.clicked.connect(self._on_button_push)
+        grid_layout.addWidget(self.btn, 4, 0, 1, 2)
+
+        # Setup layer callbacks
+        self.viewer.layers.events.inserted.connect(
+            lambda e: e.value.events.name.connect(self._on_layer_change)
+        )
+        self.viewer.layers.events.inserted.connect(self._on_layer_change)
+        self.viewer.layers.events.removed.connect(self._on_layer_change)
+        self.viewer.layers.events.removed.connect(self._check_image_layer_removed)
+        self._on_layer_change(None)
+
+        # Key bindings
+        self.viewer.bind_key('s', lambda _: self._on_button_push())
+        self.viewer.bind_key('Escape', self._on_escape)
+
+        # Viewer events
+        self.viewer.dims.events.order.connect(self._on_dimensions_changed)
+        self.viewer.dims.events.ndisplay.connect(self._on_dimensions_changed)
+    
+    def _check_image_layer_removed(self, e):
+        if self.image_layer == e.value:
+            # Reset everything if the user deleted the image layer
+            self._handle_inactive()
+
+    @property
+    def image_data(self):
+        """The image data, adjusted to handle the RGB case."""
+        if self.image_layer is None:
+            return
+        
+        if self.image_layer.data is None:
+            return
+        
+        if self.image_layer.data.ndim == 2:
+            return self.image_layer.data
+        
+        elif self.image_layer.data.ndim == 3:
+            if self.image_layer.rgb is True:
+                return np.mean(self.image_layer.data, axis=2)
+            else:
+                return self.image_layer.data
+    
+    @property
+    def is_in_3d_view(self):
+        return self.viewer.dims.ndisplay == 3
+
+    @property
+    def dims_displayed(self):
+        return list(self.viewer.dims.displayed)
+    
+    @property
+    def ndim(self):        
+        if self.image_data is None:
+            return
+        
+        if self.image_layer.rgb is True:
+            return 2
+        else:
+            return self.image_layer.data.ndim
+    
+    @property
+    def axes(self):
+        if self.is_in_3d_view:
+            return
+        
+        axes = self.dims_displayed
+        if self.ndim == 3:
+            axes.insert(0, list(set([0, 1, 2]) - set(self.dims_displayed))[0])
+        
+        return axes
+    
+    @property
+    def current_step(self):
+        """Current step, adjusted based on the layer transpose state."""
+        return np.array(self.viewer.dims.current_step)[self.axes][0]
+    
+    @property
+    def selected_label(self):
+        if self.labels_layer is None:
+            return
+        
+        return self.labels_layer.selected_label
+    
+    @selected_label.setter
+    def selected_label(self, selected_label):
+        if self.labels_layer is None:
+            return
+        
+        self.labels_layer.selected_label = selected_label
+    
+    @property
+    def image_data_slice(self):
+        """The currently visible 2D slice if the image is 3D, otherwise the image itself (if 2D)."""      
+        if self.image_data is None:
+            return
+        
+        if self.ndim == 2:
+            return self.image_data
+        
+        elif self.ndim == 3:
+            return self.image_data.transpose(self.axes)[self.current_step]
+    
+    def _on_layer_change(self, e):
+        self.cb_image.clear()
+        for x in self.viewer.layers:
+            if isinstance(x, napari.layers.Image):
+                if x.data.ndim in [2, 3]:
+                    self.cb_image.addItem(x.name, x.data)
+
+        self.cb_result.clear()
+        for x in self.viewer.layers:
+            if isinstance(x, napari.layers.Labels):
+                self.cb_result.addItem(x.name, x.data)
+
+    def _on_button_push(self):
+        if self.cb_image.currentText() == '':
+            return
+        
+        if self.is_in_3d_view:
+            show_info('Please be in 2D view mode!')
+            return
+        
+        self.image_layer = self.viewer.layers[self.cb_image.currentText()]
+        
+        if self.cb_result.currentText() == '':
+            self.result_layer = self.viewer.add_labels(np.zeros_like(self.image_data, dtype=np.int_), name='Live wire')
+        else:
+            self.result_layer = self.viewer.layers[self.cb_result.currentText()]
+        
+        self.is_active = not self.is_active  # Handle that with a button setter?
+
+        if self.is_active:
+            self._handle_active()
+        else:
+            self._handle_inactive()
+        
+    def _handle_active(self):
+        self.is_active = True
+
+        # Create a Labels layer
+        self.labels_layer = self.viewer.add_labels(np.zeros_like(self.image_data, dtype=np.int_), name='Live wire (current edit)')
+        self.labels_layer.mouse_drag_callbacks.append(self._on_mouse_click)
+        self.labels_layer.mouse_double_click_callbacks.append(self._on_press_finish_key)
+        
+        # Create the wire
+        self.wire = pyift.livewire.LiveWire(image=self.image_data_slice, sigma=1.0)
+
+        # Viewer callback
+        self.viewer.cursor.events.position.connect(self._on_cursor_move)
+        
+        # Update the button text
+        self.btn.setText('Stop live wire (s)')
+        
+        # Viewer text overlay
+        self.viewer.text_overlay.visible = True
+        self.viewer.text_overlay.text = 'Double-click to close the object selection.'
+
+    def _handle_inactive(self):
+        self.is_active = False
+
+        # Remove the Labels layer
+        if self._on_mouse_click in self.labels_layer.mouse_drag_callbacks:
+            self.labels_layer.mouse_drag_callbacks.remove(self._on_mouse_click)
+        
+        if self._on_press_finish_key in self.labels_layer.mouse_double_click_callbacks:
+            self.labels_layer.mouse_double_click_callbacks.remove(self._on_press_finish_key)
+
+        for idx, layer in enumerate(self.viewer.layers):
+            if layer.name == 'Live wire (current edit)':
+                self.viewer.layers.pop(idx)
+
+        # Viewer callback
+        self.viewer.cursor.events.position.disconnect(self._on_cursor_move)
+        
+        # Update the button text
+        self.btn.setText('Start live wire (s)')
+
+        # Viewer text overlay
+        self.viewer.text_overlay.visible = False
+    
+    def _on_dimensions_changed(self):
+        """Reset the livewire. Called when 3D view is toggled and when the layer gets transposed."""       
+        if self.image_data is None:
+            return
+        
+        self.wire = pyift.livewire.LiveWire(image=self.image_data_slice, sigma=1.0)
+    
+    def _on_press_finish_key(self, source_layer, e):
+        if self.is_in_3d_view:
+            return
+        
+        if self.labels_layer is None:
+            return
+        
+        if self.labels_layer.data.sum() == 0:
+            return
+        
+        if self.auto_fill_objects_cb.isChecked():
+            self.wire.close()
+            self.wire.confirm()
+
+        mask = self.wire.contour
+        if self.auto_fill_objects_cb.isChecked():
+            mask = ndi.binary_fill_holes(mask)
+        mask = mask > 0
+
+        if self.ndim == 2:
+            self.result_layer.data[mask] = self.selected_label
+        elif self.ndim == 3:
+            self.result_layer.data.transpose(self.axes)[self.current_step][mask] = self.selected_label
+        
+        self.result_layer.refresh()
+
+        if self.check_label_increment.isChecked():
+            self.selected_label += 1
+
+        self.wire = pyift.livewire.LiveWire(image=self.image_data_slice, sigma=1.0)
+
+    def _on_cursor_move(self, e):
+        if self.is_in_3d_view:
+            return
+        
+        if self.ndim == 2:
+            xpos, ypos = np.array(e.value).astype(int)
+        elif self.ndim == 3:
+            _, xpos, ypos = np.array(e.value).astype(int)[self.axes]
+        
+        # Check that the cursor is inside the image.
+        rx, ry = self.image_data_slice.shape
+        if not (0 < xpos < rx) & (0 < ypos < ry):
+            return
+        
+        # Sometimes, we get value errors for unexpected reasons, so we use a try... except block.
+        try:
+            self.wire.select([xpos, ypos])
+
+            wire_mask = self.wire.contour * self.selected_label
+
+            if self.ndim == 2:
+                self.labels_layer.data = wire_mask
+            elif self.ndim == 3:
+                self.labels_layer.data.transpose(self.axes)[self.current_step] = wire_mask
+            
+            self.labels_layer.refresh()
+
+        except ValueError:
+            return
+
+    def _on_mouse_click(self, source_layer, event):
+        if self.is_in_3d_view:
+            return
+        
+        self.wire.confirm()
+
+    def _on_escape(self, e):
+        """Cancel the wire drawing action (initialize a new wire)"""
+        if self.is_in_3d_view:
+            return
+        
+        self.wire = pyift.livewire.LiveWire(image=self.image_data_slice, sigma=1.0)
